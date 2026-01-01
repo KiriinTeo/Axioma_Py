5c1db1bceddadf16817d23993586cdc4b1381302
Eduardo Silva Pinheiro Teodoro <eduteodoro54@gmail.com>
2026-01-01 14:53:14 -0300

    dev: Testando pipeline de testes

---
+++ tests/teste_manual.py
@@ -0,0 +1,19 @@
+from application.services.dataset_service import DatasetService
+from application.services.plot_service import PlotService
+from application.use_cases.generate_plot_use_case import GeneratePlotUseCase
+
+dataset_service = DatasetService()
+plot_service = PlotService()
+
+ctx = dataset_service.load("data/exemplo.csv")
+
+use_case = GeneratePlotUseCase(plot_service)
+
+fig, ax = use_case.execute(
+    ctx=ctx,
+    plot_type="line",
+    x="coluna_x",
+    y="coluna_y"
+)
+
+fig.show()
