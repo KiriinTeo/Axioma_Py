e248c1cf8b3c8fd7dee0682ba0f3911e7b6d32c2
Eduardo Silva Pinheiro Teodoro <eduteodoro54@gmail.com>
2026-01-01 15:53:15 -0300

    dev: Novamente testando pipeline de testes

--- tests/teste_manual.py
+++ tests/teste_manual.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from application.services.dataset_service import DatasetService
 from application.services.plot_service import PlotService
 from application.use_cases.generate_plot_use_case import GeneratePlotUseCase
